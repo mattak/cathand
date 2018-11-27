548f95da7f1c4bde86528722aa1b96cc061da7cf
mattak <mattak.me@gmail.com>
2018-11-27 12:22:27 +0900

    :seedling: Apply cli command style

--- cmd/cathand/main.go
+++ cmd/cathand/main.go
@@ -1,38 +1,62 @@
 package main
 
 import (
+	"errors"
 	"github.com/mattak/cathand/pkg/cathand"
+	"github.com/urfave/cli"
+	"log"
 	"os"
 )
 
-func usage() {
-	panic(`
-usage:
-  record  <project>
-  compose <project>
-  play    <project>
-example: 
-  record  sample
-  compose sample
-  play    sample
-`)
-}
-
 func main() {
-	if len(os.Args) < 3 {
-		usage()
+	app := cli.NewApp()
+	app.Name = "cathand"
+	app.Usage = "record then play just like you on android device"
+	app.Version = "0.0.1"
+	app.Commands = []cli.Command{
+		{
+			Name:      "record",
+			Aliases:   []string{"r"},
+			Usage:     "record touch events",
+			ArgsUsage: "[project_name]",
+			Action: func(c *cli.Context) error {
+				if len(c.Args()) <= 0 {
+					return errors.New("ERROR: missing project name")
+				}
+				cathand.CommandRecord(c.Args().First())
+				return nil
+			},
+		},
+		{
+			Name:      "compose",
+			Aliases:   []string{"c"},
+			Usage:     "compose playable touch events from recorded data",
+			ArgsUsage: "[project_name]",
+			Action: func(c *cli.Context) error {
+				if len(c.Args()) <= 0 {
+					return errors.New("ERROR: missing project name")
+				}
+				cathand.CommandCompose(c.Args().First())
+				return nil
+			},
+		},
+		{
+			Name:      "play",
+			Aliases:   []string{"p"},
+			Usage:     "play playable touch events",
+			ArgsUsage: "[project_name]",
+			Action: func(c *cli.Context) error {
+				if len(c.Args()) <= 0 {
+					return errors.New("ERROR: missing project name")
+				}
+				cathand.CommandPlay(c.Args().First())
+				return nil
+			},
+		},
 	}
 
-	command := os.Args[1]
-	projectName := os.Args[2]
-
-	if command == "record" {
-		cathand.CommandRecord(projectName)
-	} else if command == "play" {
-		cathand.CommandPlay(projectName)
-	} else if command == "compose" {
-		cathand.CommandCompose(projectName)
-	} else {
-		usage()
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
