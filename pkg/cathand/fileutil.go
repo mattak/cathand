e96b6bfe74dd59cfe050f73bb69a9db0a98512e6
mattak <mattak.me@gmail.com>
2019-01-29 12:14:52 +0900

    :seedling: issues/3 Define ListUpFilePathes

--- pkg/cathand/fileutil.go
+++ pkg/cathand/fileutil.go
@@ -2,7 +2,10 @@ package cathand
 
 import (
 	"github.com/labstack/gommon/log"
+	"io/ioutil"
 	"os"
+	"path"
+	"strings"
 )
 
 func MakeDirectory(outdir string) {
@@ -20,6 +23,53 @@ func RemoveFile(outdir string) {
 	if err != nil {
 		log.Fatal("cannot remove directory " + outdir)
 	}
+}
+
+func ListUpFilePathes(dir string) ([]string, error) {
+	files, err := ioutil.ReadDir(dir)
+
+	if err != nil {
+		return nil, err
+	}
+
+	results := []string{}
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue;
+		}
+		results = append(results, path.Join(dir, file.Name()))
+	}
+
+	return results, nil
+}
+
+func ListUpFileNamesWithoutExtension(dir string) ([]string, error) {
+	files, err := ioutil.ReadDir(dir)
+
+	if err != nil {
+		return nil, err
+	}
+
+	results := []string{}
+
+	for _, file := range files {
+		if file.IsDir() {
+			continue;
+		}
+		results = append(results, TrimExtension(file.Name()))
+	}
+
+	return results, nil
+}
+
+func TrimExtension(filename string) string {
+	index := strings.LastIndex(filename,".")
+	if index == -1 {
+		return filename
+	}
+
+	return filename[0:index]
 }
 
 func FileExists(directory string) bool {
