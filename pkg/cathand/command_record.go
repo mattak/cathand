9b2cbdb2314428e08fad353e9fc1fb00cc2a588b
mattak <mattak.me@gmail.com>
2019-01-30 15:40:43 +0900

    :seedling: issues/8 Remove unused device folder

--- pkg/cathand/command_record.go
+++ pkg/cathand/command_record.go
@@ -2,25 +2,8 @@ package cathand
 
 import (
 	"io/ioutil"
-	"strings"
 	"sync"
 )
-
-func GetInputDevices() []string {
-	inputsRaw := RunWaitWrite("adb", "shell", "ls", "/dev/input")
-	inputsStr := strings.Replace(NormalizeLineFeed(inputsRaw), "\n", " ", -1)
-
-	inputs := strings.Split(inputsStr, " ")
-	results := []string{}
-
-	for i := 0; i < len(inputs); i++ {
-		if len(inputs[i]) >= 2 {
-			results = append(results, inputs[i])
-		}
-	}
-
-	return results
-}
 
 func CommandRecord(project Project, recordOption RecordOption) {
 	sdcardProject := NewProject(project.Name, "/sdcard/")
@@ -31,7 +14,6 @@ func CommandRecord(project Project, recordOption RecordOption) {
 
 	// 1. create directory: sample.record
 	MakeDirectory(project.RootDir)
-	MakeDirectory(project.DeviceDir)
 	MakeDirectory(project.VideoDir)
 	RunWait("adb", "shell", "rm", "-r", sdcardProject.VideoDir)
 	RunWait("adb", "shell", "mkdir", "-p", sdcardProject.VideoDir)
@@ -48,19 +30,11 @@ func CommandRecord(project Project, recordOption RecordOption) {
 	eventWg.Add(1)
 	recordWg.Add(1)
 
-	// 3.1. go data.bytes
-	inputDevices := GetInputDevices()
-	eventDataResults := make([]chan []byte, len(inputDevices))
-	for i := 0; i < len(inputDevices); i++ {
-		eventDataResults[i] = make(chan []byte)
-		go RunWaitKillWrite(eventDataResults[i], &eventWg, "adb", "shell", "cat", "/dev/input/"+inputDevices[i])
-	}
-
-	// 3.2. go getevent.log
+	// 3.1. go getevent.log
 	eventTextResult := make(chan []byte)
 	go RunWaitKillWrite(eventTextResult, &eventWg, "adb", "shell", "getevent", "-t")
 
-	// 3.3. go recording
+	// 3.2. go recording
 	go RecordContinuously(&sdcardProject, sdcardVideoFilesCh, recordOption)
 
 	// 4. receive signal & stop 3 shells
@@ -69,9 +43,6 @@ func CommandRecord(project Project, recordOption RecordOption) {
 
 	// 5. write results
 	ioutil.WriteFile(project.EventFile, NormalizeLineFeedBytes(<-eventTextResult), 0644)
-	for i := 0; i < len(inputDevices); i++ {
-		ioutil.WriteFile(project.DeviceFile(inputDevices[i]), <-eventDataResults[i], 0644)
-	}
 
 	// 6. pull files
 	RunWait("adb", "pull", sdcardProject.VideoDir, project.VideoDir)
